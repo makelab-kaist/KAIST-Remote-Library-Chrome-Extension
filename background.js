fa20680991b6468fa6780b876d85b9f5822f5703
Andrea Bianchi <andrea@kaist.ac.kr>
2025-01-17 00:48:38 +0900

    Bump up to 0.1.1

--- background.js
+++ background.js
@@ -3,11 +3,11 @@ chrome.action.onClicked.addListener(async (tab) => {
     args: [tab.url],
     target: { tabId: tab.id },
     func: async (url = '') => {
-      if (url.substring(0, 7) != 'http://') {
-        document.location = 'https://libra.kaist.ac.kr/_Lib_Proxy_Url/' + url;
-      } else {
+      if (url.substring(0, 7) === 'http://') {
         const urlLen = url.length;
         url = url.substring(7, urlLen); // strip 'http://'
+        document.location = 'https://libra.kaist.ac.kr/_Lib_Proxy_Url/' + url;
+      } else {
         document.location = 'https://libra.kaist.ac.kr/_Lib_Proxy_Url/' + url;
       }
     },
