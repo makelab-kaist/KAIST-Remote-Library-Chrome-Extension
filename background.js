fb22bac59bcb167c7e2cb0f7be9db61a936b102e
Andrea Bianchi <andrea@kaist.ac.kr>
2025-01-17 00:46:50 +0900

    Simplified

--- background.js
+++ background.js
@@ -1,21 +1,15 @@
 chrome.action.onClicked.addListener(async (tab) => {
-  chrome.scripting
-    .executeScript({
-      args: [tab.url],
-      target: { tabId: tab.id },
-      func: async (url = '') => {
-        let targetUrl = '';
-        if (url.substring(0, 7) != 'http://') {
-          targetUrl = 'https://libra.kaist.ac.kr/_Lib_Proxy_Url/' + url;
-        } else {
-          const urlLen = url.length;
-          u = url.substring(7, urlLen);
-          targetUrl = 'https://libra.kaist.ac.kr/_Lib_Proxy_Url/' + url;
-        }
-        document.location = targetUrl;
-      },
-    })
-    .then(() => {
-      console.log('Script executed');
-    });
+  chrome.scripting.executeScript({
+    args: [tab.url],
+    target: { tabId: tab.id },
+    func: async (url = '') => {
+      if (url.substring(0, 7) != 'http://') {
+        document.location = 'https://libra.kaist.ac.kr/_Lib_Proxy_Url/' + url;
+      } else {
+        const urlLen = url.length;
+        url = url.substring(7, urlLen); // strip 'http://'
+        document.location = 'https://libra.kaist.ac.kr/_Lib_Proxy_Url/' + url;
+      }
+    },
+  });
 });
